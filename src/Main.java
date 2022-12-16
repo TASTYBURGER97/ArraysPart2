c31217211b85b39433196aff3b84be299bd5e7df
TASTYBURGER97 <tihonov.artyom1997@yandex.ru>
2022-12-17 02:16:23 +0400

    Помогите пожалуйста : не понимаю в чём ошибка, при запуске кода вылазают огромные строки ошибок

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,64 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ////////Task1 /////////
+        int[] array = arrayRandom (30);
+        int sum = 0;
+        for (int number : array) {
+            sum += number;
+        }
+        System.out.println("Сумма траты за месяц составила: " + sum);
+
+
+        ////////Task2 /////////
+
+        int min = array[0];
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println("Минимальное значение = " + min + "Максимальное значние = " + max);
+
+        Arrays.sort(array);
+        System.out.println("Минимальный расход в течении месяца = %d, максимальный расход = %d \n", array[0], array[array.length-1]);
+
+        System.out.println("Средний расход за месяц составил: %.2f \n", (double)sum/ array.length);
+
+        ////////Task3 /////////
+        String string = "Иван Иванович Иванов";
+        char[] stringToChar = string.toCharArray();
+        System.out.println(Arrays.toString(stringToChar));
+        for(int i = stringToChar.length - 1; i>=0; i--) {
+            System.out.println(stringToChar[i]);
+
+        }
+
+
+
     }
+
+    public static int[] arrayRandom(int length) {
+        Random random = new Random();
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = random.nextInt(100_000) + 100_000;
+        }
+        return array;
+    }
+
 }
+
+
+
+
+
+
+
+
